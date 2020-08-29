868fbc702bde1768dd04720f51c3bd1d53d45aa6
yarasaleh <pyara-100@hotmail.com>
2020-08-29 16:46:17 +0300

    test

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 '''
-Tests for jwt flask app.
+Tests for jwt flask app
 '''
 import os
 import json
@@ -31,7 +31,7 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
